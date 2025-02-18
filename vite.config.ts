3e55ca435c70ff0e068530e25006155262f87bac
freestrokes <freestrokes.hk@gmail.com>
2025-02-18 19:40:55 +0900

    feat: modify proxy settings

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,54 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: {
-    port : 5173
+    port : 5173,
+    proxy: {
+      // 문자열만:
+      // http://localhost:5173/foo
+      //   -> http://localhost:4567/foo
+      // '/foo': 'http://localhost:4567',
+
+      // 옵션과 함께:
+      // http://localhost:5173/api/bar
+      //   -> http://jsonplaceholder.typicode.com/bar
+      '/api': {
+        target: 'http://jsonplaceholder.typicode.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        // SSL 인증서 검증 무시
+        secure: false,
+        // WebSocket 프로토콜 사용
+        ws: true,
+      },
+
+      // 정규식(RegExp)과 함께:
+      // http://localhost:5173/fallback/
+      //   -> http://jsonplaceholder.typicode.com/
+      // '^/fallback/.*': {
+      //   target: 'http://jsonplaceholder.typicode.com',
+      //   changeOrigin: true,
+      //   rewrite: (path) => path.replace(/^\/fallback/, '')
+      // },
+
+      // 프락시 인스턴스 사용
+      // '/api': {
+      //   target: 'http://jsonplaceholder.typicode.com',
+      //   changeOrigin: true,
+      //   configure: (proxy, options) => {
+      //     // proxy 변수에는 'http-proxy'의 인스턴스가 전달됩니다
+      //   }
+      // },
+
+      // 웹소켓 또는 socket.io 프락시:
+      // ws://localhost:5173/socket.io
+      //   -> ws://localhost:5174/socket.io
+      // `rewriteWsOrigin`을 사용할 때는 주의하세요.
+      // CSRF 공격에 노출될 수 있습니다.
+      // '/socket.io': {
+      //   target: 'ws://localhost:5174',
+      //   ws: true,
+      //   rewriteWsOrigin: true,
+      // }
+    }
   }
 })
