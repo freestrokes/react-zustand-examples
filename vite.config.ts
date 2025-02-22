adcc910b8fbc581122e3ee478206ed42969bd0f2
freestrokes <freestrokes.hk@gmail.com>
2025-02-22 21:02:26 +0900

    feat: modify vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,7 @@
-import { defineConfig } from 'vite'
+import {
+  defineConfig,
+  // loadEnv
+} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths';
 
@@ -57,3 +60,26 @@ export default defineConfig({
     }
   }
 })
+
+// env 변수 사용 예시
+// mod warning 처리 필요
+// export default ({ mode }) => {
+// 	// 현재 작업 디렉터리의 `mode`를 기반으로 env 파일을 불러옴
+// 	// 세 번째 매개변수를 ''로 설정하면 `VITE_` 접두사에 관계없이 모든 환경 변수를 불러옴
+// 	const env = loadEnv(mode, process.cwd(), '');
+//
+// 	return defineConfig({
+// 		plugins: [react()],
+// 		define: {
+// 			__APP_ENV__: JSON.stringify(env.APP_ENV),
+// 		},
+// 		server: {
+// 			proxy: {
+// 				'/api': {
+// 					target: `${env.SERVER_URL}:${env.SERVER_PORT}`,
+// 					changeOrigin: true,
+// 				},
+// 			},
+// 		},
+// 	});
+// };
