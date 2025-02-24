67488172e97107e7bf632a65e79455def57dfdf3
freestrokes <freestrokes.hk@gmail.com>
2025-02-24 22:07:17 +0900

    feat: modify useTestStore.ts

--- src/stores/useTestStore.ts
+++ src/stores/useTestStore.ts
@@ -2,9 +2,18 @@ import { create } from 'zustand'
 
 const useTestStore = create((set) => ({
 	bears: 0,
-	increasePopulation: () => set((testState: any) => ({ bears: testState.bears + 1 })),
-	removeAllBears: () => set({ bears: 0 }),
-	updateBears: (newBears: any) => set({ bears: newBears }),
+	increasePopulation: () =>
+		set((testState: any) => ({
+			bears: testState.bears + 1
+		})),
+	removeAllBears: () =>
+		set({
+			bears: 0
+		}),
+	updateBears: (newBears: any) =>
+		set({
+			bears: newBears
+		}),
 }))
 
 export default useTestStore;
