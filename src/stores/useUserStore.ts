45d5eb2858ea5b59fa14c8e32f1308db40673d14
freestrokes <freestrokes.hk@gmail.com>
2025-02-26 01:00:57 +0900

    feat: modify useUserStore.ts

--- src/stores/useUserStore.ts
+++ src/stores/useUserStore.ts
@@ -2,7 +2,8 @@ import { create } from "zustand";
 import { User } from "@/types/User";
 import { UserState } from "@/states/UserState";
 
-const useUserStore = create((set) => ({
+const useUserStore = create<UserState>((set) => ({
+	user: {} as User,
 	updateUser: (user: User) =>
 		set((state: UserState) => ({
 			user: { ...state.user, ...user },
